69026e8de10500ce38c1adcade76281a2c0ca82a
jonguk <jonguk1221@gmail.com>
2019-05-28 08:55:23 +0900

    config server ssl

--- src/main/java/mirotic/demospringboot/DemoSpringBootApplication.java
+++ src/main/java/mirotic/demospringboot/DemoSpringBootApplication.java
@@ -1,10 +1,35 @@
 package mirotic.demospringboot;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoSpringBootApplication {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Demo Test";
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoSpringBootApplication.class, args);
