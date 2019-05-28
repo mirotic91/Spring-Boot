b143a7104e529ba3a10f21317461b8967f7c7510
jonguk <jonguk1221@gmail.com>
2019-05-29 08:10:17 +0900

    config server http2

--- src/main/java/mirotic/demospringboot/DemoSpringBootApplication.java
+++ src/main/java/mirotic/demospringboot/DemoSpringBootApplication.java
@@ -1,11 +1,7 @@
 package mirotic.demospringboot;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,19 +12,6 @@ public class DemoSpringBootApplication {
     @GetMapping("/test")
     public String test() {
         return "Demo Test";
-    }
-
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-        return tomcat;
-    }
-
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(8080);
-        return connector;
     }
 
     public static void main(String[] args) {
