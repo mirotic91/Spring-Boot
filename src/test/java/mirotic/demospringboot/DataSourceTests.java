fa8c759a284d80326b77d2c373e3f91290d617cc
jonguk <jonguk1221@gmail.com>
2019-08-09 08:46:34 +0900

    DataSource separation

--- src/test/java/mirotic/demospringboot/DataSourceTests.java
+++ src/test/java/mirotic/demospringboot/DataSourceTests.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
@@ -13,21 +13,20 @@ import java.sql.DatabaseMetaData;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-@SpringBootTest
+@DataJpaTest
 @RunWith(SpringRunner.class)
-public class DemoSpringBootApplicationTests {
+public class DataSourceTests {
 
     @Autowired
     DataSource dataSource;
 
     @Test
-    public void PostgreSQLConfig() throws Exception{
+    public void H2Config() throws Exception{
         DatabaseMetaData metaData = dataSource.getConnection().getMetaData();
         log.debug("Database URL[{}]", metaData.getURL());
         log.debug("Database DriverName[{}]", metaData.getDriverName());
         log.debug("Database DriverVersion[{}]", metaData.getDriverVersion());
 
-        assertThat(metaData.getDriverName()).isEqualTo("PostgreSQL JDBC Driver");
+        assertThat(metaData.getDriverName()).isEqualTo("H2 JDBC Driver");
     }
-
 }
